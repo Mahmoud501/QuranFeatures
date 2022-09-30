b6529717f45235cf12476626c882a47faf930bb2
Mahmoud501 <deep2012_1st@yahoo.com>
2022-09-30 15:27:15 +0200

    save

---
+++ TestApp/Model/AzkarModel.swift
@@ -0,0 +1,16 @@
+//
+//  AzkarModel.swift
+//  TestApp
+//
+//  Created by Mahmoud Zaki on 30/09/2022.
+//
+
+import Foundation
+
+class AzakrModel {
+    
+    var id: Int?
+    var name: String?
+    var desc: String?
+    var count: Int?
+}
---
+++ TestApp/Model/PrayTimeModel.swift
@@ -0,0 +1,13 @@
+//
+//  PrayTimeModel.swift
+//  TestApp
+//
+//  Created by Mahmoud Zaki on 30/09/2022.
+//
+
+import Foundation
+
+class PrayTimeModel {
+    
+    
+}
